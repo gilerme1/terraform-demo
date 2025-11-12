01ba1f66308119fdffc047660e47d9a175e6d89c
Guillermo_Rivero <gilerme1980@gmail.com>
2025-11-12 11:15:31 -0300

    feat: mi primer instancia de prueba

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
-// 1. Configuración del Proveedor (Provider)
+# 1. Configuración del Proveedor
 provider "aws" {
   region = "us-east-1"
 }
 
-// 2. Definición del Recurso (Resource)
+# 2. Crear una instancia EC2 gratuita
 resource "aws_instance" "servidor_web" {
-  ami           = "ami-0abcdef1234567890" // ID de una AMI de Amazon Linux (ejemplo)
+  ami           = "ami-0cae6d6fe6048ca2c" # Amazon Linux 2023(free tier)
   instance_type = "t2.micro"
+
   tags = {
     Name = "MiPrimerServidor"
   }
